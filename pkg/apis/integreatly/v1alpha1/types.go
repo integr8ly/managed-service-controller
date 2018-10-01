d341ad57e9aad52cbbfb231cb29ae3b88ca99934
John Roche <jroche@redhat.com>
2018-10-01 11:22:58 +0100

    INITIAL COMMIT

---
+++ pkg/apis/integreatly/v1alpha1/types.go
@@ -0,0 +1,29 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type ManagedServiceNamespaceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []ManagedServiceNamespace `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type ManagedServiceNamespace struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              ManagedServiceNamespaceSpec   `json:"spec"`
+	Status            ManagedServiceNamespaceStatus `json:"status,omitempty"`
+}
+
+type ManagedServiceNamespaceSpec struct {
+	// Fill me
+}
+type ManagedServiceNamespaceStatus struct {
+	// Fill me
+}
